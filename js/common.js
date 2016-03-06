bedcfc53eeaa28a1df768d797362b6969cb4c0d4
shazgorn <shazgorn@yandex.ru>
2016-03-06 23:58:16 +0300

    clear compiled files

--- js/common.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  jQuery('#login_form').submit(function(e) {
-    e.preventDefault();
-    localStorage.setItem('user_id', $('#login').val());
-    return location.pathname = '/game';
-  });
-
-}).call(this);
