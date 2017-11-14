380fdd399d10d8210b14b3840894b773b58b2e28
shazgorn <shazgorn@yandex.ru>
2017-11-15 01:35:40 +0300

    add log_in helper to js_spec

--- spec/js_spec.rb
+++ spec/js_spec.rb
@@ -4,11 +4,26 @@ require 'capybara/rspec'
 require 'capybara/webkit'
 require 'i18n'
 
+module PlayHelper
+  def log_in
+    visit "/"
+    login = 'capybara' + Time.now.hour.to_s + Time.now.min.to_s + Time.now.sec.to_s
+    within("#login-form") do
+      fill_in 'login', with: login
+    end
+    expect(I18n.t('log_in')).to eq('Войти')
+    click_button I18n.t('log_in')
+    expect(page).to have_content(I18n.t('Exit'))
+    login
+  end
+end
+
 RSpec.configure do |c|
   Capybara.javascript_driver = :webkit
   Capybara.app_host = 'http://0.0.0.0:9292/'
   Capybara.run_server = false
   c.include TimeHelper
+  c.include PlayHelper
   c.include Capybara::DSL
   c.before(:example) {
     I18n.load_path = Dir[
@@ -20,20 +35,16 @@ RSpec.configure do |c|
 end
 
 RSpec.describe "Gaming process", :js => true do
-  fit "is playing" do
-    visit "/"
-    login = 'capybara' + Time.now.hour.to_s + Time.now.min.to_s + Time.now.sec.to_s
-    within("#login-form") do
-      fill_in 'login', with: login
-    end
-    expect(I18n.t('log_in')).to eq('Войти')
-    click_button I18n.t('log_in')
-    expect(page).to have_content(I18n.t('Exit'))
+  it "is testing user info" do
+    login = log_in
     sleep(1) # wait init_map
     expect(find('#user-info-nickname-info').text).to eq(login)
     expect(find('#user-info-glory-info').text).to eq("#{Config.get('START_GLORY')}/#{Config.get('START_MAX_GLORY')}")
+  end
+
+  it "is renaming" do
+    login = log_in
     unit_name = find('#unit-info-list > .unit-info:first-of-type .unit-name-info').text
-    # TODO: move starter unit type to Config
     expect(unit_name).to eq(I18n.t('Heavy Infantry'))
     find('#unit-info-list > .unit-info:first-of-type .unit-name-info').double_click()
     expect(page).to have_css('#unit-info-list > .unit-info:first-of-type .unit-name-info input')
@@ -50,15 +61,11 @@ RSpec.describe "Gaming process", :js => true do
     find('.player-hero'){|div| expect(div['title']).to eq(new_unit_name)}
   end
 
+  it "is playing" do
+  end
+
   it "is building" do
-    # TODO: move login to helper
-    visit "/"
-    within("#login-form") do
-      fill_in 'login', with: 'capybara' + Time.now.hour.to_s + Time.now.min.to_s + Time.now.sec.to_s
-    end
-    expect(I18n.t('log_in')).to eq('Войти')
-    click_button I18n.t('log_in')
-    expect(page).to have_content(I18n.t('Exit'))
+    login = log_in
     find('.inventory-item-settlers').click()
     expect(page).to have_content(I18n.t('res_settlers_action_label'))
     click_button(I18n.t('res_settlers_action_label'))
